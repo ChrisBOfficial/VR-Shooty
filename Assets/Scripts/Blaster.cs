9225890de85dec4e11a58764ba38e033b485f2d7
ChrisBOfficial <officialchrisb@icloud.com>
2020-02-05 14:59:59 -0600

    Added Unity stuff

---
+++ Assets/Scripts/Blaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blaster : MonoBehaviour
+{
+    public GameObject m_ProjectilePrefab = null;
+
+    private ProjectilePool m_ProjectilePool = null;
+
+    private void Awake()
+    {
+        m_ProjectilePool = new ProjectilePool(m_ProjectilePrefab, 10);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float m_Lifetime = 5.0f;
+
+    private Rigidbody m_Rigidbody = null;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        SetInnactive();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SetInnactive();
+    }
+
+    public void Launch(Blaster blaster)
+    {
+        // Position
+        // TODO: Setup with barrel
+        transform.position = blaster.transform.position;
+        transform.rotation = blaster.transform.rotation;
+
+        // Activate
+        gameObject.SetActive(true);
+
+        // Fire, and track
+        // TODO: Setup force
+        m_Rigidbody.AddRelativeForce(Vector3.forward * 10, ForceMode.Impulse);
+        StartCoroutine(TrackLifetime());
+    }
+
+    private IEnumerator TrackLifetime()
+    {
+        yield return new WaitForSeconds(m_Lifetime);
+        SetInnactive();
+    }
+
+    public void SetInnactive()
+    {
+        m_Rigidbody.velocity = Vector3.zero;
+        m_Rigidbody.angularVelocity = Vector3.zero;
+
+        gameObject.SetActive(false);
+    }
+}
