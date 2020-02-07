9119df1e91e4928f8cfec7d3558e8e50d6e9734e
ChrisBOfficial <officialchrisb@icloud.com>
2020-02-07 15:23:36 -0600

    Finished teleportation, added two more targets

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -10,6 +10,8 @@ public class Teleporter : MonoBehaviour
 
     private SteamVR_Behaviour_Pose m_Pose = null;
     private bool m_HasPosition = false;
+    private bool m_IsTeleporting = false;
+    private float m_FadeTime = 0.0f;
 
     private void Awake()
     {
@@ -31,12 +33,41 @@ public class Teleporter : MonoBehaviour
 
     private void TryTeleport()
     {
+        // Check for valid position, and if already teleporting
+        if (!m_HasPosition || m_IsTeleporting)
+        {
+            return;
+        }
 
+        // Get camera rig, and head position
+        Transform cameraRig = SteamVR_Render.Top().origin;
+        Vector3 headPosition = SteamVR_Render.Top().head.position;
+
+        // Figure out translation
+        Vector3 groundPosition = new Vector3(headPosition.x, cameraRig.position.y, headPosition.z);
+        Vector3 translateVector = m_Pointer.transform.position - groundPosition;
+
+        // Move
+        StartCoroutine(MoveRig(cameraRig, translateVector));
     }
 
     private IEnumerator MoveRig(Transform cameraRig, Vector3 translation)
     {
-        yield return null;
+        // Flag
+        m_IsTeleporting = true;
+
+        // Fade to black
+        SteamVR_Fade.Start(Color.black, m_FadeTime, true);
+
+        // Apply translation
+        yield return new WaitForSeconds(m_FadeTime);
+        cameraRig.position += translation;
+
+        // Fade to clear
+        SteamVR_Fade.Start(Color.clear, m_FadeTime, true);
+
+        // De-flag
+        m_IsTeleporting = false;
     }
 
     private bool UpdatePointer()
