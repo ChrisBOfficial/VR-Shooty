978027e59339a38806bbbbcfc4f0c6d65e973715
khpolivanov <31524380+khpolivanov@users.noreply.github.com>
2020-02-10 15:37:17 -0600

    Added enemies that shoot at player

--- Assets/Scripts/Enemy_Spawn.cs
+++ Assets/Scripts/Enemy_Spawn.cs
@@ -6,6 +6,7 @@ public class Enemy_Spawn : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private GameObject enemy;
+    private GameObject enemy1;
 
     // Update is called once per frame
     void Update()
@@ -16,6 +17,12 @@ public class Enemy_Spawn : MonoBehaviour
 
     		float angle = Random.Range(0, 360);
     		enemy.transform.Rotate(0, angle, 0);
+
+    		enemy1 = Instantiate(enemyPrefab) as GameObject;
+    		enemy1.transform.position = new Vector3(3, 1, 3);
+
+    		float angle1 = Random.Range(0, 360);
+    		enemy1.transform.Rotate(0, angle1, 0);
     	}
         
     }
---
+++ Assets/Scripts/matPaintBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class matPaintBall : MonoBehaviour
+{
+	public float speed = 10.0f;
+	public int damageNum = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) {
+    	playerInfo player = other.GetComponent<playerInfo>();
+
+    	if(player != null) {
+    		player.Hurt(damageNum);
+    	}
+    	Destroy(this.gameObject);
+
+    }
+}
